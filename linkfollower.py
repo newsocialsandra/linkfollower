4a9dd7f98fd66f1c3fddc9ebb8268ea6c1846c14
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-12-25 15:22:41 +0100

    First commit

---
+++ linkfollower.py
@@ -0,0 +1,22 @@
+import urllib
+from BeautifulSoup import *
+
+url = raw_input("Enter url:")
+count = raw_input("Enter count:")
+count = int(count)
+position = raw_input("Enter position:")
+position = int(position)
+
+while count > 0:
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    tags = soup('a')
+    linklist = []
+    for t in tags:
+        linklist.append(t.get('href', None))
+    url = linklist[position]
+    print url
+    count -= 1
+    del linklist[:]
+
+print "The last url is", url
